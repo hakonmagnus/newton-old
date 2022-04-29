b7f6cd0e285d6d78735070927a8a07050311ad29
Hákon Hjaltalín <hakonmagnus45@gmail.com>
2022-04-29 00:22:00 +0000

    platform header

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -3,5 +3,3 @@
 Thank you for your interest in contributing to the Newton Framework. Please note that we
 have a [Code of Conduct](./CODE_OF_CONDUCT.md) which you should read before participating
 in this project.
-
-
---
+++ newton/include/newton/newton.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Hákon Hjaltalín.
+//
+// This project is licensed under the MIT license. Please see
+// LICENSE or go to https://opensource.org/licenses/MIT for
+// more information.
+
+#pragma once
+
+#include "newton/base/NtDefs.h"
